e71df18c3b957fd87d5427c0538f1f4c838db11d
devansh rathod <rathoddevansh53@gmail.com>
2024-11-21 15:23:49 +0530

    profile set

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
            <div className='flex items-center gap-2 cursor-pointer group relative'>
             <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
             <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-grey-600 z-20 hidden group-hover:block'>
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
               <div>
                 <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>MyProfile</p>
                 <p onClick={()=>navigate('my-apoimmentt')} className='hover:text-black cursor-pointer'>MyApointments</p>
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { assets } from '../assets/assets';
 const MyProfile = () => {
+
+  const [userData,serUserData] = useState({
+    name:"devansh rathod",
+    Image:assets.profile_pic,
+    email:"rathodevansh@gmail.com",
+    phone:"+91077-240-121-93",
+    address:{
+      line1:" B-93/102 tarsh enclave ",
+      line2:"samrth park colony umaria"
+    },
+    gender:'Male',
+    DOB:'2004-01-27'
+  })
+  const [isEdit,setIsEdit] = useState(false)
   return (
     <div>
-      
+      <img src={userData.Image} alt="" />
+     {
+      isEdit ? <input type="text" value={userData.name} onChange={e=>serUserData(prev => ({...prev,name:e.target.value}))} /> : <p>{userData.name}</p>
+     }
+    <hr/>
+    <div>
+      <p>CONTACT INFORMATION</p>
+      <div>
+        <p>Email id:</p>
+        <p>{userData.email}</p>
+      </div>
+    </div>
     </div>
   )
 }
-
 export default MyProfile
